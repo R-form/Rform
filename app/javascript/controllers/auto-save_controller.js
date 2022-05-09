5d37055e0f9fad2fb08c3953209569572470374d
krystal <krystal93002232@gmail.com>
2022-05-09 18:51:06 +0800

    add update autosave

--- app/javascript/controllers/auto-save_controller.js
+++ app/javascript/controllers/auto-save_controller.js
@@ -2,20 +2,11 @@ import { Controller } from "stimulus";
 import Rails from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = ["form", "button"];
+  static targets = ["form", "button", "question_value"];
 
   connect() {
-    this.localStorageKey = window.location;
-    // TODO del
-    console.log(this.localStorageKey);
-    // this.setFormData();
+    // console.log(this.question_valueTarget);
   }
-
-  // clearLocalStorage() {
-  //   if (localStorage.getItem(this.localStorageKey) != null) {
-  //     localStorage.removeItem(this.localStorageKey);
-  //   }
-  // }
 
   getFormData() {
     const form = new FormData(this.formTarget);
@@ -25,7 +16,7 @@ export default class extends Controller {
       if (pair[0] != "authenticity_token") {
         data.push([pair[0], pair[1]]);
         // TODO del
-        console.log(pair);
+        // console.log(pair);
       }
     }
 
@@ -37,43 +28,20 @@ export default class extends Controller {
   //   console.log(this.buttonTarget);
   // }
 
-  saveToLocalStorage(e) {
+  autoSaveToSurvey(e) {
     const data = this.getFormData();
-    // localStorage.setItem(this.localStorageKey, JSON.stringify(data));
+
     // TODO del
-    console.log(data);
+    // console.log(data);
 
     const id = this.element.children[2].dataset.id;
     if (id == null) {
       this.formTarget.submit();
     } else {
+      // if () {
+
+      // }
       this.formTarget.submit();
-      // Rails.ajax({
-      //   type: "patch",
-      //   url: `/surveys/${id}`,
-      //   data: data,
-      //   dataType: "JSON",
-      //   success: (resp) => {
-      //     console.log(resp);
-      //   },
-      //   error: (err) => {
-      //     console.log(err);
-      //   },
-      // });
     }
   }
-
-  // setFormData() {
-  //   if (localStorage.getItem(this.localStorageKey) != null) {
-  //     const data = JSON.parse(localStorage.getItem(this.localStorageKey));
-
-  //     const form = this.formTarget;
-  //     Object.entries(data).forEach((word) => {
-  //       let name = word[0];
-  //       let value = word[1];
-  //       let input = form.querySelector(`[name='${name}']`);
-  //       input && (input.value = value);
-  //     });
-  //   }
-  // }
 }
