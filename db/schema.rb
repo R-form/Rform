e67e19c097be88b578aa4549547dc30f351cc2c5
JonHuang <s72503013@gmail.com>
2022-05-11 23:34:12 +0800

    dead code remove

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_05_07_090337) do
-
-
+ActiveRecord::Schema.define(version: 2022_05_11_100134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +67,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_090337) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "required"
-    t.integer "position"
     t.index ["survey_id"], name: "index_questions_on_survey_id"
   end
 
@@ -88,8 +84,7 @@ ActiveRecord::Schema.define(version: 2022_05_07_090337) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
-    t.integer "position"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_surveys_on_user_id"
   end
 
