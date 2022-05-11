a601f7fd88c8c0b71a1bc870457b97371310ed96
郁芳 <yufang@Yvonne-MacBook-Air.local>
2022-05-11 15:17:31 +0800

    create homepage root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
   
-  
   private
     def not_authenticated 
       redirect_to login_path, alert: "Please login first"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'homepage#index'
+
   get 'password_resets/create'
   get 'password_resets/edit'
   get 'password_resets/update'
-  root :to => 'users#index'
+
   resources :users
 
   get 'login' => 'user_sessions#new', :as => :login
@@ -18,7 +20,6 @@ Rails.application.routes.draw do
   post "oauth/callback" => "oauths#callback"
   get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
-  get "survey_style", to:"survey#style"
 
   resources :password_resets, only: [:new, :create, :edit, :update]
 
