d4dcc25c6478778d598cd59fc705da8ae67b3340
JonHuang <s72503013@gmail.com>
2022-05-16 14:19:42 +0800

    optimiztion code

--- app/javascript/controllers/surveys_controller.js
+++ app/javascript/controllers/surveys_controller.js
@@ -2,25 +2,20 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = ["short_url"];
-  static values = { index: String };
 
   share(e) {
     e.preventDefault();
-    const url = `${location.protocol}${location.host}${e.srcElement.closest("a").dataset.url}`
-    console.log(url);
-    this.short_urlTarget.value = `${url}`
+    const url = `${location.protocol}${location.host}${
+      e.target.closest("a").dataset.url
+    }`;
+    this.short_urlTarget.value = url;
   }
   copy(e) {
-    e.preventDefault()
-    console.log("複製成功");
-    const copyText = `${this.short_urlTarget.value}`
-    navigator.clipboard.writeText(copyText)
-        .then(() => {
-            console.log('內容已複製');
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-
-    }
+    e.preventDefault();
+    const copyText = `${this.short_urlTarget.value}`;
+    navigator.clipboard
+      .writeText(copyText)
+      .then((resp) => {})
+      .catch((err) => {});
+  }
 }
