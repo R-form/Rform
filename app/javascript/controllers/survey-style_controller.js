32d77b4ac4b25652dbd3bee515b0b1adc12c61d0
krystal <krystal93002232@gmail.com>
2022-05-17 11:33:15 +0800

    fix answers autosave

--- app/javascript/controllers/survey-style_controller.js
+++ app/javascript/controllers/survey-style_controller.js
@@ -1,31 +1,23 @@
 import { Controller } from "stimulus";
 import Rails from "@rails/ujs";
 
-
 export default class extends Controller {
-  static targets = [];
-
-  connect() {
-    // console.log(document.querySelector("form").children[2].dataset.id);
-     }
-
   option(e) {
-    const id = document.querySelector("form").children[2].dataset.id
-    const form = document.querySelector("form")
-    const font_style =  e.target.value
+    const id = document.querySelector("form").children[2].dataset.id;
+    const form = document.querySelector("form");
+    const font_style = e.target.value;
 
-    
-    switch(font_style){
+    switch (font_style) {
       case "font-sans":
-        form.classList.remove("font-mono","font-serif");
-        form.classList.add("font-sans")
+        form.classList.remove("font-mono", "font-serif");
+        form.classList.add("font-sans");
         break;
       case "font-mono":
-        form.classList.remove("font-serif","font-sans");
+        form.classList.remove("font-serif", "font-sans");
         form.classList.add("font-mono");
         break;
       case "font-serif":
-        form.classList.remove("font-mono","font-sans");
+        form.classList.remove("font-mono", "font-sans");
         form.classList.add("font-serif");
         break;
     }
@@ -36,11 +28,9 @@ export default class extends Controller {
     Rails.ajax({
       type: "patch",
       url: `/surveys/${id}/font_style`,
-      data: data ,
+      data: data,
       success: (resp) => {},
       error: (err) => {},
     });
-
   }
-
 }
