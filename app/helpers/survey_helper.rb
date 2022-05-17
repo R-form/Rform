37843bc26a2fea0a3826849e0ccd0552502d2c88
Chao-Hung, Wan <qchwan@gmail.com>
2022-05-17 22:47:40 +0800

    create qrcode link

--- app/helpers/survey_helper.rb
+++ app/helpers/survey_helper.rb
@@ -1,13 +1,3 @@
 module SurveyHelper
-require "rqrcode"
-  def qrcode(path)
-    rqrcode = RQRCode::QRCode.new(path)
-    svg = rqrcode.as_svg(
-      color: "000",
-      shape_rendering: "crispEdges",
-      module_size: 11,
-      standalone: true,
-      use_path: true
-    ).html_safe
-  end
+
 end
