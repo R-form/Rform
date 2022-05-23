4d29edf9976a7893d11462f26c398bf28c70b2b5
Nikka <kyra11024@gmail.com>
2022-05-23 16:43:45 +0800

    use action instead of addEventListener

--- app/javascript/controllers/stats_controller.js
+++ app/javascript/controllers/stats_controller.js
@@ -1,15 +1,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["show_responses", "show_responses_button"];
+  static targets = ["show_responses"];
 
-  connect() {
-    this.show_responses_buttonTarget.addEventListener("click", () => {
-        if (this.show_responsesTarget.className === "hidden"){
-            this.show_responsesTarget.classList.remove("hidden")
-          } else {
-            this.show_responsesTarget.classList.add("hidden")
-          }
-    })
+  hideAndShow() {
+    if (this.show_responsesTarget.className === "hidden"){
+      this.show_responsesTarget.classList.remove("hidden")
+    } else {
+      this.show_responsesTarget.classList.add("hidden")
+    }
   }
 }
