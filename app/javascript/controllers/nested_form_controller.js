e4244d448b21eb1838ea2ce6f6573e2e2f7290f1
JonHuang <s72503013@gmail.com>
2022-05-23 15:10:44 +0800

    remove console.log in nested_form_controller.js

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -66,7 +66,6 @@ export default class extends Controller {
   }
 
   change_background(event) {
-    console.log(this.change_bgTarget);
     this.change_bgTarget.submit();
     event.value = "";
   }
