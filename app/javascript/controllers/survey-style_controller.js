342a5784b69cd44f927f5e063c77b64b97653676
郁芳 <yufang@Yvonne-MacBook-Air.local>
2022-05-23 17:58:49 +0800

    sidebar design

--- app/javascript/controllers/survey-style_controller.js
+++ app/javascript/controllers/survey-style_controller.js
@@ -4,7 +4,40 @@ import Huebee from "huebee"
 import "huebee/dist/huebee.min"
 
 export default class extends Controller {
-  static targets = ['survey_id', 'form'];
+  static targets = ['survey_id', 'form', 'color'];
+
+  connect() {
+    console.log(this.colorTarget);
+    new Huebee(this.colorTarget,{
+      staticOpen: true
+    })
+  }
+
+  pick_color(e){
+    const id = this.survey_idTarget.dataset.id
+    const theme_color = e.target.value
+    console.log(theme_color);
+
+    // const color_field = this.themeTarget
+
+    // color_field.style['background-color'] = theme_color
+
+    // console.log(color_field);
+
+    // console.log(theme_color);
+
+    // const data = new FormData();
+    // data.append("theme", theme_color);
+
+
+    // Rails.ajax({
+    //   type: "patch",
+    //   url: `/surveys/${id}/theme`,
+    //   data: data,
+    //   success: ({ message }) => {},
+    //   error: (err) => {},
+    // });
+  }
 
   select_font(e) {
     const id = this.survey_idTarget.dataset.id;
@@ -20,29 +53,6 @@ export default class extends Controller {
       type: "patch",
       url: `/surveys/${id}/font_style`,
       data,
-      success: ({ message }) => {},
-      error: (err) => {},
-    });
-  }
-
-  pick_color(){
-    const id = document.querySelector("form").children[2].dataset.id;
-    const theme_color = this.colorTarget.value
-    const color_field = this.themeTarget
-    color_field.style['background-color'] = theme_color
-
-    // console.log(color_field);
-
-    console.log(theme_color);
-
-    const data = new FormData();
-    data.append("theme", theme_color);
-
-
-    Rails.ajax({
-      type: "patch",
-      url: `/surveys/${id}/theme`,
-      data: data,
       success: ({ message }) => {},
       error: (err) => {},
     });
