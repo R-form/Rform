29114bbdf5dcee092cbd46c36f9bd8d638138d53
Nikka <kyra11024@gmail.com>
2022-05-25 21:45:36 +0800

    move canvas_area from id to class

--- app/javascript/controllers/stats_controller.js
+++ app/javascript/controllers/stats_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
 
   connect() {
     for (let i=0; i<this.canvas_barTargets.length; i++) {
-      this.canvas_barTargets[i].closest("#canvas_area").classList.remove("hidden") //default display with bar chart
+      this.canvas_barTargets[i].closest(".canvas_area").classList.remove("hidden") //default display with bar chart
     }
   }
 
@@ -23,9 +23,9 @@ export default class extends Controller {
     var currentCanvasTarget = this.canvas_barTarget // just a initial value
 
     // clear all charts first
-    this.canvas_barTargets[targetIndex].closest("#canvas_area").classList.add("hidden")
-    this.canvas_pieTargets[targetIndex].closest("#canvas_area").classList.add("hidden")
-    this.canvas_lineTargets[targetIndex].closest("#canvas_area").classList.add("hidden")
+    this.canvas_barTargets[targetIndex].closest(".canvas_area").classList.add("hidden")
+    this.canvas_pieTargets[targetIndex].closest(".canvas_area").classList.add("hidden")
+    this.canvas_lineTargets[targetIndex].closest(".canvas_area").classList.add("hidden")
       
     switch (selectedChartType) {
       case 'pie':
@@ -41,7 +41,7 @@ export default class extends Controller {
     }
       
     if (currentCanvasTarget.attributes.getNamedItem("data-chart-type-value").nodeValue===selectedChartType) {
-      currentCanvasTarget.closest("#canvas_area").classList.remove("hidden") // only show the type we selected
+      currentCanvasTarget.closest(".canvas_area").classList.remove("hidden") // only show the type we selected
     }
   }
 }
