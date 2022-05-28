955528d13c0c1f2a3017488019cc114779e91ca0
郁芳 <yufang@Yvonne-MacBook-Air.local>
2022-05-29 02:36:18 +0800

    save skip_question_id in answer table & append the id in response html

---
+++ app/javascript/controllers/select-question_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "stimulus";
+import Rails from "@rails/ujs";
+import Swal from "sweetalert2";
+import 'sweetalert2/src/sweetalert2.scss'
+
+export default class extends Controller {
+  static targets = ['survey_id'];
+
+    get_question(e){
+    const survey_id = this.survey_idTarget.dataset.id
+    const question_id = e.target.dataset.question_id
+    const answer_id = e.target.dataset.answer_id
+    const skip_to_question_id = e.target.value
+
+    const data = new FormData();
+    data.append("question_id", question_id);
+    data.append("answer_id", answer_id);
+    data.append("skip_to_question_id", skip_to_question_id);
+
+    Rails.ajax({
+      type: "patch", 
+      url: `/surveys/${survey_id}/skip_to_question_id`,
+      data,
+      success: () => {
+        console.log("成功");
+      },
+      error: () => {
+        console.log("失敗");
+      },
+      });
+
+    }
+}
+
+ 
+
+
---
+++ app/javascript/controllers/skip-logic-modal_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from "stimulus";
+import Rails from "@rails/ujs";
+import Swal from "sweetalert2";
+import 'sweetalert2/src/sweetalert2.scss'
+
+export default class extends Controller {
+  static targets = ['survey_id'];
+
+  open_alert(e) {
+    e.preventDefault()
+    const survey_id = this.survey_idTarget.dataset.id
+    const question_id = e.target.closest(".question").dataset.question_id;
+    const answer_id = e.target.closest(".answer").firstElementChild.value;
+  
+    Rails.ajax({
+      type: "get", 
+      url: `/surveys/${survey_id}/questions_list`,
+      success: ({ message }) => {
+        let select_option  = "" 
+        message.forEach((element)=>{
+          const { title, id } = element;
+          if(id != question_id){
+          select_option +=`<option value='${id}'>${title}</option>`
+          }
+        })      
+        Swal.fire({
+          title: '<strong>請選擇要前往的題目</strong>',
+          html: 
+          `<select
+            data-question_id = '${question_id}'
+            data-answer_id = '${answer_id}'
+            data-action='change->select-question#get_question'>
+            <option>請選擇</option>${select_option}
+          </select>`,
+          showCloseButton: true,
+          showCancelButton: true,
+          confirmButtonText:'<i class="fa fa-thumbs-up"></i> 儲存',
+          confirmButtonAriaLabel: 'Thumbs up, great!',
+          cancelButtonAriaLabel: 'Thumbs down'
+        })  
+    },
+      error: () => {
+        console.log("失敗");
+      },
+    });
+
+  }
+
+}
+
+ 
