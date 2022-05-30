aba5e42ba48da38784170ed2aa1a54f7bb841fbf
郁芳 <yufang@Yvonne-MacBook-Air.local>
2022-05-31 01:05:27 +0800

    keep selected option after refresh page

--- app/javascript/controllers/select-question_controller.js
+++ app/javascript/controllers/select-question_controller.js
@@ -1,64 +1,53 @@
-import { Controller } from "stimulus";
-import Rails from "@rails/ujs";
-
+import { Controller } from "stimulus"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ['survey_id'];
+  static targets = ["survey_id"]
 
-    initialize() {
-      this.selectItem = {}
-    }
+  get_question(e) {
+    const survey_id = this.survey_idTarget.dataset.id
+    const question_id = e.target.dataset.question_id
+    const answer_id = e.target.dataset.answer_id
+    const skip_to_question_id = e.target.value
 
-    get_question(e){
-      const survey_id = this.survey_idTarget.dataset.id
-      const question_id = e.target.dataset.question_id
-      const answer_id = e.target.dataset.answer_id
-      const skip_to_question_id = e.target.value
+    const data = new FormData()
+    data.append("question_id", question_id)
+    data.append("answer_id", answer_id)
+    data.append("skip_to_question_id", skip_to_question_id)
 
-      this.selectItem = {
-        survey_id,
-        question_id,
-        answer_id,
-        skip_to_question_id
-      }
+    Rails.ajax({
+      type: "patch",
+      url: `/surveys/${survey_id}/skip_to_question_id`,
+      data,
+      success: () => {
+        console.log("儲存成功")
+      },
+      error: () => {
+        console.log("儲存失敗")
+      },
+    })
+  }
 
-      const data = new FormData();
-      data.append("question_id", question_id);
-      data.append("answer_id", answer_id);
-      data.append("skip_to_question_id", skip_to_question_id);
-      
-      Rails.ajax({
-        type: "patch", 
-        url: `/surveys/${survey_id}/skip_to_question_id`,
-        data,
-        success: () => {
-          console.log("儲存成功");
-        },
-        error: () => {
-          console.log("儲存失敗");
-        },
-        }); 
+  remove_question() {
+    const select = document.querySelector("#select_for_skip_logic")
+    const survey_id = select.dataset.survey_id
+    const question_id = select.dataset.question_id
+    const answer_id = select.dataset.answer_id
 
+    const data = new FormData()
+    data.append("question_id", question_id)
+    data.append("answer_id", answer_id)
 
-    } 
-
-    remove_question(){
-    const data = new FormData();
-      data.append("question_id", this.selectItem.question_id);
-      data.append("answer_id", this.selectItem.answer_id);
-      data.append("skip_to_question_id", this.selectItem.skip_to_question_id);
-      
-      Rails.ajax({
-        type: "delete", 
-        url: `/surveys/${ this.selectItem.survey_id}/remove_skip_to_question_id`,
-        data,
-        success: () => {
-          console.log("移除成功");
-        },
-        error: () => {
-          console.log("移除失敗");
-        },
-      }); 
-    }
-
+    Rails.ajax({
+      type: "delete",
+      url: `/surveys/${survey_id}/remove_skip_to_question_id`,
+      data,
+      success: () => {
+        console.log("移除成功")
+      },
+      error: () => {
+        console.log("移除失敗")
+      },
+    })
+  }
 }
