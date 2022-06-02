075b226793008bba835867993b36dcd074b2257b
krystal <krystal93002232@gmail.com>
2022-06-02 18:18:25 +0800

    fix responses submit button

--- app/javascript/controllers/responses_controller.js
+++ app/javascript/controllers/responses_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["question", "previous_question", "next_question", "submit"]
+  static targets = ["question", "previous_question", "next_question", "submit", "survey_title"]
 
   connect() {
+    this.survey_titleTarget.classList.remove("hidden")
     this.questionTarget.classList.remove("hidden")
     this.next_questionTargets[this.next_questionTargets.length - 1].classList.add("hidden")
     this.previous_questionTarget.classList.add("hidden")
@@ -24,6 +25,7 @@ export default class extends Controller {
 
     question.classList.add("hidden")
     this.skip_from_question_id.push(question_id)
+
     const next_question = question.nextElementSibling
     const skip_question = this.element.querySelector(`div [data-question_id='${this.skip_to_question_id}']`)
     const final_question_id = this.questionTargets[this.questionTargets.length - 1].dataset.question_id
@@ -40,10 +42,12 @@ export default class extends Controller {
       }
     }
     this.skip_to_question_id = ""
+    this.survey_titleTarget.classList.add("hidden")
   }
 
   previous(e) {
     e.preventDefault()
+    this.submitTarget.classList.add("hidden")
     const question_count = this.skip_from_question_id.length
     const previous_question_id = this.skip_from_question_id[Number(question_count) - 1]
 
@@ -51,6 +55,9 @@ export default class extends Controller {
     question.classList.add("hidden")
     const skip_from_question = this.element.querySelector(`div [data-question_id='${previous_question_id}']`)
     skip_from_question.classList.remove("hidden")
+    if (previous_question_id == this.skip_from_question_id[0]) {
+      this.survey_titleTarget.classList.remove("hidden")
+    }
     this.skip_from_question_id.splice(-1)
   }
 }
