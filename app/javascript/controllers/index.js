86ded0d9d2afd748706bf4c4addb643977892598
krystal <krystal93002232@gmail.com>
2022-06-06 03:40:56 +0800

    add notice style

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,11 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Notification from "stimulus-notification"
 
-const application = Application.start();
-const context = require.context("controllers", true, /_controller\.js$/);
-application.load(definitionsFromContext(context));
+const application = Application.start()
+application.register("notification", Notification)
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
