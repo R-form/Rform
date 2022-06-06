b088ec496594886f4a9c30821cd4f14464aa55fd
郁芳 <yufang@Yvonne-MacBook-Air.local>
2022-06-06 17:54:58 +0800

    add header navbar RWD

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static values = { isOpen: Boolean }
 
-  showNavBar(e) {
+  showNavbar(e) {
     e.preventDefault()
     this.isOpenValue = !this.isOpenValue
     const slideover = document.querySelector(".slideover")
