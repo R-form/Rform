f2277694cd12c18e8512fcb88598338069914ef8
Chao-Hung, Wan <qchwan@gmail.com>
2022-06-07 21:28:19 +0800

    set up action_mailer for environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,12 +38,21 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { :host => "127.0.0.1:3000" }
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => ENV["MAIL_DOMAIN"] }
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               ENV["MAIL_DOMAIN"],
+    user_name:            ENV["MAIL_USERNAME"],
+    password:             ENV["MAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
