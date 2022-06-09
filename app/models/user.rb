1b81971942a6766c90e3e053f010ceef10b7aa45
krystal <krystal93002232@gmail.com>
2022-06-09 17:43:38 +0800

    add field_with_errors scss

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   authenticates_with_sorcery! do |config|
     config.authentications_class = Authentication
   end
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6, message: "密碼長度需大於6碼" }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { message: "信箱錯誤"}
   has_many :surveys, -> { order(position: :asc) } , :dependent => :destroy
   has_many :authentications, :dependent => :destroy
   has_many :orders
